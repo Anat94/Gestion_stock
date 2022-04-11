507abcd3ae57f167fb42931d521586b8551d74f7
Anat94 <anatole.babin@epitech.eu>
2022-04-11 15:47:36 +0200

    [ADD] Init repo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Gestion de stock</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a2db49bfe2.js" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body class ="search_page">
+    <form method="post" action="verif.php">
+        <h1 class="title_search"> Gestion de stock</h1>
+        <div class="input">
+            <input type="article" name ="article" placeholder="Article">
+            <input type="taille"  name ="taille" placeholder="Taille">
+        </div>
+        <div align="center">
+            <button type="submit">Rechercher</button>
+        </div>
+    </form>
+    <script src="" async defer></script>
+</body>
+
+</html>
